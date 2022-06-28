6c133dd79e379d25e6eabb2a4d4cfdf97dd2ed6b
prakhar <prakher1992@gmail.com>
2022-06-28 09:30:36 +0530

    custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+
+)
+# from django.core.validators import RegexValidator
+
 
 # Create your models here.
+# phone_validator = RegexValidator(r"^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)"
+#                                  r"\s?-?\s?(\(?\d{3}\)?)"
+#                                  r"\s?-?\s?(\(?\d{4}\)?)?$",
+#                                  "The phone number provided is invalid")
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    phoneno = models.CharField(max_length=12, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
