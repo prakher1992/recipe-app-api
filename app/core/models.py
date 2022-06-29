77e9f07d62baa4a176b1424a614de1b3bb22b854
prakhar <prakher1992@gmail.com>
2022-06-29 08:22:53 +0530

    user model validation

--- app/core/models.py
+++ app/core/models.py
@@ -18,15 +18,27 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
+
     email = models.EmailField(max_length=255, unique=True)
-    phoneno = models.CharField(max_length=12, unique=True)
+    #phoneno = models.CharField(max_length=12, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
