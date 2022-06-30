699347017fd96f4bac7f9f414bd5eab9258898b0
prakhar <prakher1992@gmail.com>
2022-06-30 08:57:24 +0530

    custom user admin page

--- app/core/models.py
+++ app/core/models.py
@@ -35,10 +35,11 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=255, unique=True)
-    #phoneno = models.CharField(max_length=12, unique=True)
+    # phoneno = models.CharField(max_length=12, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
